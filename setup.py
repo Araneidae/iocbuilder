e1a94e24b84317bb62e66e754862cc755021ce28
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2009-01-06 09:29:09 +0000

    IOC builder: Rename package to iocbuilder
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/iocbuilder@26645 e099a375-04f9-0310-9d5f-a741eaff62e1

--- setup.py
+++ setup.py
@@ -9,19 +9,28 @@ except:
     version = "development"
 
 setup(
-    # install_requires allows you to import a specific version of a module in your scripts 
-    # setup_requires lets us use the site specific settings for installing scripts
-    setup_requires = ["dls.environment==1.0"],
-    # name of the module
-    name = "dls.builder",
-    # version: over-ridden by the release script
+#     # install_requires allows you to import a specific version of a module in
+#     # your scripts, setup_requires lets us use the site specific settings for
+#     # installing scripts
+#     setup_requires = ["dls.environment==1.0"],
+#     # name of the module
+#     name = "iocbuilder",
+#     # version: over-ridden by the release script
+#     version = version,
+#     packages = [
+#         "dls", "dls.builder",
+#         "dls.builder.hardware", "dls.builder.modules", "dls.builder.defaults"],
+#     package_dir = {
+#         'dls': 'dls',
+#         'dls.builder': 'src'},
+# #        'dls.builder.hardware': 'src/hardware'},
+#     namespace_packages = ['dls'],
+    name = 'iocbuilder',
     version = version,
-    packages = ["dls","dls.builder", "dls.builder.hardware"],
-    package_dir = {
-        'dls': 'dls',
-        'dls.builder': 'src',
-        'dls.builder.hardware': 'src/hardware'},
-    namespace_packages = ['dls'],
+    description = 'IOC builder',
+    author = 'Michael Abbott',
+    author_email = 'Michael.Abbott@diamond.ac.uk',
+    
     include_package_data = True,
     zip_safe = False
     )
