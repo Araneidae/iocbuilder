f2189e8649aa3bfa25e360bd48a639fd32ed860c
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2009-07-06 07:08:49 +0000

    IOC builder: Some more work on arginfo.py.
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/iocbuilder@33494 e099a375-04f9-0310-9d5f-a741eaff62e1

--- iocbuilder/arginfo.py
+++ iocbuilder/arginfo.py
@@ -13,28 +13,24 @@ class ArgInfo(object):
 
     class mybase(ModuleBase):
         def __init__(self, arg1, arg2=1):
-            # make arg appear as self.arg for all __init__ arguments
-            self.__dict__.update(locals())
-            ModuleBase.__init__(self)
+            self.__super.__init__(self)
+            ... do other stuff...
 
         ArgInfo = makeArgInfo(__init__,
             arg1 = (str, 'Description for arg1'),
-            arg2 = (int, 'Description for arg2')
-        )    
+            arg2 = (int, 'Description for arg2'))    
     
-    ArgInfo has the format:
-    
-    ArgInfo = [
-        ('arg1', str, 'Description for arg1'),
-        ('arg2', int, 'Description for arg2', 1)
-    ]
 
+    Here ArgInfo contains the following fields:
+
+        .descriptions       Dictionary mapping argument names to meta-data
+        .required_names     List of mandatory arguments (without defaults)
+        .default_names      List of arguments with default values
+        .default_values     List of default values of arguments
+        .optional_names     List of optional arguments.
     '''
 
     def __init__(self, __source=None, __optional=[], __method=True, **descs):
-        '''
-        '''
-
         self.descriptions = descs
         self.optional_names = __optional
 
@@ -52,43 +48,55 @@ class ArgInfo(object):
                 self.required_names = names
                 self.default_names = []
                 self.default_values = []
+
+            if varkw:
+                # Add any names in descs that we've not already accounted for
+                # to the list of required names.  We sort the list so that
+                # the ordering is at least predictable.
+                self.required_names += sorted(list(
+                    set(descs) - set(names) - set(__optional)))
+            assert varkw or not __optional, \
+                'Unusable optional arguments: %s' % __optional
         else:
+            # Somewhat special case for taking the arguments from a list of
+            # names.  These are treated as all mandatory.
             if __source:
                 self.required_names = list(__source)
             else:
                 self.required_names = []
             self.default_names = []
             self.default_values = []
-            varkw = False
 
-
-        # Some set arithmetic to facilitate checking and calculations
-        set_names = set(self.required_names) | set(self.default_names)
-        set_optional = set(__optional)
-        set_descs = set(descs)
-        missing = (set_names | set_optional) - set_descs
-        extra_names = set_descs - set_names - set_optional
-        duplicates = set_names & set_optional
-
-        # Any extra names are added to the list of required names
-        self.required_names += sorted(list(extra_names))
+        self.__validate()
         
-        # Validate the arguments.
-        assert not duplicates, \
-            'Duplication of arguments: %s' % list(duplicates)
-        assert not missing, 'No description for arguments: %s' % list(missing)
-        assert varkw or not extra_names, \
-            'Unexpected optional arguments: %s' % list(extra_names)
-        assert varkw or not __optional, \
-            'Unusable optional arguments: %s' % __optional
 
     def __validate(self):
-        assert len(set(self.optional_names)) == len(self.optional_names), \
+        # Validates the complete ArgInfo data.
+        set_required = set(self.required_names)
+        set_default  = set(self.default_names)
+        set_optional = set(self.optional_names)
+
+        # First check for duplicates in all the lists of names.
+        assert len(set_optional) == len(self.optional_names), \
             'Duplicate optional names'
-        assert len(set(self.required_names)) == len(self.required_names), \
+        assert len(set_required) == len(self.required_names), \
             'Duplicate required names'
-        assert len(set(self.default_names)) == len(self.default_names), \
+        assert len(set_default) == len(self.default_names), \
             'Duplicate default names'
+        # Check for overlaps between the lists
+        assert \
+            not (set_optional & set_required) and \
+            not (set_required & set_default)  and \
+            not (set_optional & set_default), 'Overlapping arguments?'
+
+        # Check that all the names are described
+        all_names = set_required | set_default | set_optional
+        set_descs = set(self.descriptions)
+        assert all_names <= set_descs, \
+            'Arguments %s not described' % (all_names - set_descs)
+        assert set_descs <= all_names, \
+            'Descriptions for unknown arguments %s' % (set_descs - all_names)
+            
 
         
     def Names(self, excludes=[]):
@@ -104,10 +112,11 @@ class ArgInfo(object):
         object.  Any overlap of names is treated as an error.'''
         result = ArgInfo()
         result.descriptions = dict(self.descriptions, **other.descriptions)
-        result.optional_names = self.optional_names + other.optional_names
         result.required_names = self.required_names + other.required_names
         result.default_names  = self.default_names  + other.default_names
         result.default_values = self.default_values + other.default_values
+        result.optional_names = self.optional_names + other.optional_names
+        self.__validate()
         return result
         
 
@@ -128,22 +137,25 @@ def addArgInfo(__method=False, **descs):
 #  Information about individual arguments.
 
 
+__all__ += ['Simple', 'Choice', 'Enum', 'Ident', 'List', 'Sevr']
+
 _simpleTypes = [int, str, float, bool]
 
 class ArgType(object):
-    def __init__(self, desc, typ, validator=None, **extras):
-        self.desc = desc
+    _extras = ['values', 'labels', 'ident']
+    def __init__(self, desc, typ, **extras):
+        self.desc = str(desc)
         self.typ = typ
-        self.validator = validator
-        self.__dict__.update(extras)
-        
+        for k in extras:
+            assert k in self._extras, "%s is not one of %s" % (k, self._extras)
+        self.__dict__.update(extras)        
             
-def Simple(desc, typ, validator = None):
+def Simple(desc, typ):
     # just a simple type
     assert typ in _simpleTypes, \
         "%s is not a supported simple type %s" % (typ, _simpleTypes)    
     desc = "%s %s" % (desc, typ)
-    return ArgType(typ, desc, validator)
+    return ArgType(desc, typ)
     
 def Choice(desc, values, vlabels = None):
     # a choice of different values, with optional different labels
@@ -153,106 +165,50 @@ def Choice(desc, values, vlabels = None):
     for v in values:
         assert typ == type(v), \
             "Value '%s' doesn't have same type as '%s'" % (v, values[0])
-    desc = "%s %s" % (desc, typ)
+    desc = "%s %s\nValues:\n\t" % (desc, typ)
     if vlabels is None:
-        vlabels = values    
-        argdescs = values
+        descs = descs + "\n\t".join(map(str,values))
+        return ArgType(typ, desc, labels = values)
     else:
         assert len(values) == len(vlabels), \
             "Value labels %s do not have the same length as values %s" % \
             (vlables, values)
         argdescs = [ "%s: %s" % x for x in zip(vlabels, values) ]        
-    descs = "%s\nValues:\n\t" % descs + "\n\t".join(argdescs)
-    return ArgType(typ, desc, args = zip(vlabels,values))
+        descs = descs + "\n\t".join(argdescs)
+        return ArgType(typ, desc, values = values, labels = labels)
 
 def Enum(desc, values):
     # a choice of different values, stores the index
-    return Choice(desc, values, range(len(values)))
+    return Choice(desc, range(len(values)), values)
 
 def Ident(desc, typ):
     # a choice of identifiers of a particular type
     assert issubclass(typ, ModuleBase), \
         "Identifier lookup only allowed on ModuleBase subclasses"
     descs = "%s %s" % (desc, typ)        
-    return ArgType(typ, desc)
+    return ArgType(typ, desc, ident = True)
 
-def Range(desc, *vals):
-    assert len(vals) in (1, 2), \
-        "Usage: Range(desc, min, max) or Range(desc, max)"
-    typ = type(vals[0])
-    assert typ in _simpleTypes, "%s is not a supported simple type %s" % \
-        (typ, _simpleTypes)
-    ran = range(*vals)
-    if len(ran) < 11:
-        # we can probably display this as a Choice
-        return Choice(desc, ran)
-    else:
-        txt = "range [%s..%s]"%(ran[0],ran[-1])
-        def validator(x):
-            if x in ran:
-                return True
-            else:
-                return "X not in "+txt
-        return ArgType(typ, "%s <%s>"%(desc, txt), validator)                
-                                                    
-class List(object):
-    # a list of argtypes
-    def __init__(self, desc, num, func, *arg, **kwargs):
-        self.argtypes = [ func("%s %d"%(desc,i) *args, **kwargs) \
-                            for i in range(num) ]
-
-    def setDefault(self, val):
-        # set this argument to be a default value for those specified in val
-        for i,v in enumerate(val):
-            self.argtypes[i].setDefault(v)
-        # for the other ones set them to be the default typ
-        for i in range(i,len(self.argtypes)):
-            self.argtypes[i].setDefault(self.argtypes[i].typ())
-               
-    def setOptional(self):
-        # set this argument to be an optional value
-        for o in self.argtypes:  
-            o.setOptional()
-
-    def setParent(self, parent):
-        # set the parent to call getArgs on    
-        for o in self.argtypes:     
-            o.setParent(parent) 
-                                                                
-    def applyQVariant(self, variant):
-        # this avoids a dependency on Qt, applied by gui
-        for o in self.argtypes:     
-            o.applyQVariant(variant) 
-
-    # at this point the classes will get split off in the gui, so no need to
-    # implement anything else
-
-def ipAddr(val):
-    # validator for an ip address
-    errStr = "Should be of format x[xx].x[xx].x[xx].x[xx][:x[x..]]"
-    split = value.strip().split(":")
-    if len(split) not in [1,2]:
-        return errStr
-    if len(split) == 2:
-        if not split[1].isdigit():
-            return errStr
-    split = split[0].split(".")
-    if len(split) == 4:
-        return errStr
-    for i in split:
-        if not i.isdigit():
-            return errStr
-        if int(i) < 1 or int(i) > 255:
-            return errStr
-    return True
-
-def checklen(l):
-    # validator generator for length
-    def validator(val):
-        if len(val) <= l:
-            return True
-        else:
-            return "Must be less than %d characters" % l
+def List(desc, num, func, *arg, **kwargs):
+    # list of argtypes
+    return [ func("%s %d"%(desc,i) *args, **kwargs) for i in range(num) ]
 
 def Sevr(desc):
-    return Choice(["NO_ALARM","MINOR","MAJOR","INVALID"], desc)
+    # choice of possible values for SEVR field
+    return Choice(desc, ["NO_ALARM","MINOR","MAJOR","INVALID"])
+
+
+class test1(object):
+    def __init__(self, a=1, b="string"):
+        pass
+    ArgInfo = makeArgInfo(__init__,
+        a = Simple("Arg 1", int),
+        b = Simple("Arg 2", str))
+
+class test2(object):
+    def __init__(self, c=12, d="string2"):
+        pass
+    ArgInfo = makeArgInfo(__init__,
+        c = Simple("Arg 3", float),
+        d = Simple("Arg 4", bool))
+        
+subclasses = [test1, test2]
