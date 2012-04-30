b7b767aee01a009e21ba9967748b2a0567838ac5
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2012-04-30 10:32:46 +0000

    IOC builder: Add IpAttach to EPICS_BASE, extend StartupCommand
    Add IpAttach device to enable the secondary network port on MVME5500 cards.
    Also extend StartupCommand to enable commands to be generated inline in
    sequence rather than always at the end directly before iocInit.
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/iocbuilder@76670 e099a375-04f9-0310-9d5f-a741eaff62e1

--- iocbuilder/defaults/EPICS_BASE.py
+++ iocbuilder/defaults/EPICS_BASE.py
@@ -27,15 +27,28 @@ class epicsBase(Device):
             self.ioc_init.ioc_name.replace('-', '_')
 
 
-class StartupCommand(ModuleBase):
+class StartupCommand(Device):
     '''Add some text to the startup script'''
-    def __init__(self, command, post_init=False):
-        IocCommand(command, post_init)
+    def __init__(self, command, post_init=False, at_end = True):
         self.__super.__init__()
+        if at_end:
+            # Backwards compatible behaviour, goes at end of startup commands
+            IocCommand(command, post_init)
+        else:
+            self.command = command
+            if post_init:
+                self.PostIocInitialise = self.__print_command
+            else:
+                self.Initialise = self.__print_command
+
+    def __print_command(self):
+        print self.command
 
     ArgInfo = makeArgInfo(__init__,
         command   = Simple('Startup command', str),
-        post_init = Simple('If True, do this after iocInit', bool))
+        post_init = Simple('If True, do this after iocInit', bool),
+        at_end    = Simple('If not set command generated in sequence', bool))
+
 
 class EpicsEnvSet(ModuleBase):
     '''Set a variable in the EPICS environment'''
@@ -47,3 +60,50 @@ class EpicsEnvSet(ModuleBase):
     ArgInfo = makeArgInfo(__init__,
         key   = Simple('Variable to set', str),
         value = Simple('Value to set it to', str))
+
+
+def dotted_to_ip(dotted):
+    quads = map(int, dotted.split('.'))
+    assert len(quads) == 4, 'Ip address must have four numbers'
+    assert min(quads) >= 0 and max(quads) < 256, 'Invalid numeric range'
+    return quads[3] | (quads[2] << 8) | (quads[1] << 16) | (quads[0] << 24)
+
+
+class IpAttach(Device):
+    '''Enables secondary network attached to secondary network port on MVME5500
+    card.'''
+
+    def __init__(self, address,
+            mask = '255.255.255.0', device = 'wancom', port = 0):
+        self.__super.__init__()
+        dotted_to_ip(address)       # Just validate address
+        self.address = address
+        self.mask = dotted_to_ip(mask)
+        self.device = device
+        self.port = port
+
+    def Initialise(self):
+        print 'ipAttach(%(port)d, "%(device)s")' % self.__dict__
+        print 'ifMaskSet("%(device)s%(port)d", 0x%(mask)08x)' % self.__dict__
+        print 'ifAddrSet("%(device)s%(port)d", "%(address)s")' % self.__dict__
+
+    ArgInfo = makeArgInfo(__init__,
+        address = Simple('IP address to bind to secondary network'),
+        mask = Simple('Netmask for network bound to secondary port'),
+        device = Simple('Internal device name of secondary network port'),
+        port = Simple('Port number of secondary network port', int))
+
+
+class hostAdd(Device):
+    def __init__(self, host, address):
+        self.__super.__init__()
+        dotted_to_ip(address)
+        self.host = host
+        self.address = address
+
+    def Initialise(self):
+        print 'hostAdd("%(host)s", "%(address)s")' % self.__dict__
+
+    ArgInfo = makeArgInfo(__init__,
+        host = Simple("Host name to be bound to given address"),
+        address = Simple("IP address to bind to given host name"))
