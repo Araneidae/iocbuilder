9f05bfca4c607b35a870a863c4ef72ff8dd38049
tmc43 <tmc43@e099a375-04f9-0310-9d5f-a741eaff62e1>
2014-07-16 13:11:22 +0000

    iocbuilder: updating dependencies dls_dependency_tree==2.10, dls_edm==1.39
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/python/iocbuilder@117074 e099a375-04f9-0310-9d5f-a741eaff62e1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description = 'IOC builder',
     author = 'Michael Abbott',
     author_email = 'Michael.Abbott@diamond.ac.uk',
-    install_requires = ['dls_dependency_tree==2.9', 'dls_edm==1.37'],
+    install_requires = ['dls_dependency_tree==2.10', 'dls_edm==1.39'],
     packages = ['iocbuilder', 'xmlbuilder', 'toolkit'],
     package_data = { 'iocbuilder': ['defaults/*.py', 'defaults/*/*.py'],
                      'xmlbuilder': ['xeb.png'] },
